f4aee00b07b5319ffb9eaa1f79872c89642ecc0c
CCBaxter84 <troyceclark@gmail.com>
2021-01-22 09:30:09 -0700

    Solved multiply and divide

--- src/recursion.js
+++ src/recursion.js
@@ -210,12 +210,51 @@ let modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  }
 
+  if (y === 1) {
+    return x;
+  } else if (y === -1) {
+    return -x;
+  }
+
+  if (y > 1) {
+    return x + (multiply(x, y - 1));
+  } else if (y < -1) {
+    return -x + (multiply(x, y + 1));
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+  if (x === 0) {
+    return 0;
+  }
+
+  let xAbs = x < 0 ? x - x - x : x;
+  let yAbs = y < 0 ? y - y - y : y;
+
+  if (x === y && x > 0) {
+    return 1;
+  } else if (x === y && x < 0) {
+    return -1;
+  } else if (xAbs < yAbs) {
+    return 0;
+  }
+
+  if (x > 0 && y > 0) {
+    return 1 + ( divide(x - y, y) );
+  } else if (x < 0 && y < 0) {
+    return 1 + ( divide(-x + y, -y) );
+  } else if (x < 0 && y > 0 || x > 0 && y < 0) {
+    return -1 + ( divide(x + y, y) );
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
